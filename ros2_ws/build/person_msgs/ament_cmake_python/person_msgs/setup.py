a369e022ad7435b5d66616d0c166afbd04c056df
suudoohmahiro <s22C1073XQ@s.chibakoudai.jp>
2023-11-30 12:24:05 +0900

    Remove ros2_ws

--- ros2_ws/build/person_msgs/ament_cmake_python/person_msgs/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='person_msgs',
-    version='0.0.0',
-    packages=find_packages(
-        include=('person_msgs', 'person_msgs.*')),
-)
