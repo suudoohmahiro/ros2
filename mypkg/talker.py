6ba653b010275a9e137585b0ef2651a232d69375
suudoohmahiro <suudooh@hogehoge.com>
2023-12-04 17:04:56 +0900

    Modified mypkg/listener.py & mypkg/talker.py

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,18 +1,18 @@
-import rclpy                     #ROS 2のクライアントのためのライブラリ
-from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
-from std_msgs.msg import Int16   #通信の型（16ビットの符号付き整数）
+import rclpy
+from rclpy.node import Node
+from person_msgs.msg import Person
 
 rclpy.init()
-node = Node("talker")            #ノード作成（nodeという「オブジェクト」を作成）
-pub = node.create_publisher(Int16, "countup", 10)   #パブリッシャのオブジェクト作成
+node = Node("talker")
+pub = node.create_publisher(Person, "person", 10)
 n = 0
-
-def cb():          #17行目で定期実行されるコールバック関数
-    global n       #関数を抜けてもnがリセットされないようにしている
-    msg = Int16()  #メッセージの「オブジェクト」
-    msg.data = n   #msgオブジェクトの持つdataにnを結び付け
-    pub.publish(msg)        #pubの持つpublishでメッセージ送信
+def cb():
+    global n
+    msg = Person()        
+    msg.name = "須藤真央"
+    msg.age = n         
+    pub.publish(msg)
     n += 1
 
-node.create_timer(0.5, cb)  #タイマー設定
-rclpy.spin(node)            #実行（無限ループ）
+node.create_timer(0.5, cb)
+rclpy.spin(node)
